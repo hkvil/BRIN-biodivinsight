dc5efc36c1e81814f740862202d4e5d27d0b31b4
Hidayat <75840967+hkvil@users.noreply.github.com>
2024-08-25 13:25:06 +0700

    Added is_owner column

--- database/migrations/2024_08_23_124819_create_user_observation_table.php
+++ database/migrations/2024_08_23_124819_create_user_observation_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('observation_id')->constrained()->onDelete('cascade');
+            $table->boolean('is_owner')->default(false);
             $table->timestamps();
         });
     }
